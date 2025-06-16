59384a12e94e5c89ee34408eb761e15b8a0a8c77
Caro-D54 <c.donaire@alumno.um.edu.ar>
2025-06-16 20:10:36 -0300

    Iniciando Home

---
+++ front-book_reserve/src/components/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="container">
+      <h1>Bienvenido a Nexus Literario</h1>
+      <p>Explora nuestra biblioteca virtual</p>
+    </div>
+  );
+}
+export default Home;
