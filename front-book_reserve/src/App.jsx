08dfdaf147e7c0fd1287b2b477404d3a7ca52759
Caro-D54 <c.donaire@alumno.um.edu.ar>
2025-11-01 02:55:22 +0100

    Agregando estilos a booklist

--- front-book_reserve/src/App.jsx
+++ front-book_reserve/src/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./components/Home";
+import Header from "./components/Header";
+import BookList from "./components/BookList";
+import "./components/Globals.css";
 
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [currentView, setCurrentView] = useState('home');
+
   return (
-    <div>
-      <Home />
+    <div className="App">
+      <Header 
+        user={user}
+        setUser={setUser}
+        currentView={currentView}
+        setCurrentView={setCurrentView}
+      />
+      <main className="main-content">
+        {currentView === 'home' && <Home setCurrentView={setCurrentView} />}
+        {currentView === 'catalog' && <BookList user={user}/>}
+      </main>
+      <footer className="footer">
+        <div className="container">
+          <p>&copy; 2023 Biblioteca Virtual Nexus Literario</p>
+        </div>
+      </footer>
     </div>
   );
 }
