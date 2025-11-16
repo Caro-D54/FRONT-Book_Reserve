d0173df3cb79947f946de384622d6baf64dcfb3f
Caro-D54 <c.donaire@alumno.um.edu.ar>
2025-11-16 15:49:55 +0100

    conectando front y back

--- front-book_reserve/src/api/axiosInstance.js
+++ front-book_reserve/src/api/axiosInstance.js
@@ -1,10 +1,19 @@
 import axios from "axios";
 
+const getApiBase = () => {
+    try {
+        const env = typeof process !== 'undefined' ? process.env.REACT_APP_API_URL : {};
+        return env?.REACT_APP_API_URL || 'http://127.0.0.1:8000/api/';
+    } catch {
+        return 'http://127.0.0.1:8000/api/';
+    }
+}; 
 const axiosInstance = axios.create({
-    baseURL: ProcessingInstruction.env.REACT_APP_API_URL || 'http://localhost:8080/api/',
+    baseURL: ProcessingInstruction.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api/',
     headers: {
         'Content-Type': 'application/json',
-    }
+    },
+    withCredentials: false,
 });
 
 axiosInstance.interceptors.request.use(
